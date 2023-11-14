7b44cd36ef058c5c6e8140eadb96871778bf1e93
강대은 <eodms4334@naver.com>
2023-11-14 11:41:44 +0900

    관광지 정보 수정중

--- EnjoyTrip-vue/src/components/attraction/AttractionList.vue
+++ EnjoyTrip-vue/src/components/attraction/AttractionList.vue
@@ -1,4 +1,5 @@
 <script setup>
+import { ref, onMounted } from "vue";
 import { useRoute, useRouter } from "vue-router";
 const router = useRouter();
 
@@ -11,24 +12,82 @@ const moveAttractionDetail = () => {
   <div id="trip">
     <div id="star">
       <div class="snowBall_img" alt="스노우볼">
-        <img id="TripFoodButton" src="@/assets/cityAttraction/TripFoodButton.png" alt="" />
-        <img id="TripFestivalButton" src="@/assets/cityAttraction/TripFestivalButton.png" alt="" />
-        <img id="TripChildHatButton" src="@/assets/cityAttraction/TripChildHatButton.png" alt="" />
+        <img
+          id="TripFoodButton"
+          src="@/assets/cityAttraction/TripFoodButton.png"
+          alt=""
+        />
+        <img
+          id="TripFestivalButton"
+          src="@/assets/cityAttraction/TripFestivalButton.png"
+          alt=""
+        />
+        <img
+          id="TripChildHatButton"
+          src="@/assets/cityAttraction/TripChildHatButton.png"
+          alt=""
+        />
 
         <select class="header-searchSelect">
           <option value="All">All</option>
-          <option value="arts-craft">음식점</option>
+          <option value="arts-craft">가게명</option>
           <option value="address">위치</option>
           <option value="menu">메뉴</option>
         </select>
         <div class="search-container">
-          <input type="text" class="search-input" placeholder="검색어를 입력하세요" />
-          <button class="search-button">검색</button>
+          <input
+            type="text"
+            class="search-input"
+            placeholder="검색어를 입력하세요"
+          />
+          <!-- 버튼에 @click="" 추가하기-->
+          <button class="search-button" type="button">검색</button>
         </div>
 
-        <img id="TripSnowButton1" src="@/assets/cityAttraction/TripSnowButton.png" alt="" />
-        <img id="TripSnowButton2" src="@/assets/cityAttraction/TripSnowButton.png" alt="" />
-        <img id="TripSnowButton3" src="@/assets/cityAttraction/TripSnowButton.png" alt="" />
+        <div class="img0">
+          <img
+            id="TripSnowButton0"
+            src="@/assets/cityAttraction/TripListAllButtonImg.png"
+            alt=""
+          />
+          <div class="TripSnowButton0_text">
+            <h3>All</h3>
+          </div>
+        </div>
+
+        <div class="img1">
+          <div class="spotLight1"></div>
+          <img
+            id="TripSnowButton1"
+            src="@/assets/cityAttraction/TripSnowButton.png"
+            alt=""
+          />
+          <div class="TripSnowButton1_text">
+            <h3>Food</h3>
+          </div>
+        </div>
+
+        <div class="img2">
+          <img
+            id="TripSnowButton2"
+            src="@/assets/cityAttraction/TripSnowButton.png"
+            alt=""
+          />
+          <div class="TripSnowButton2_text">
+            <h3>Festival</h3>
+          </div>
+        </div>
+
+        <div class="img3">
+          <img
+            id="TripSnowButton3"
+            src="@/assets/cityAttraction/TripSnowButton.png"
+            alt=""
+          />
+          <div class="TripSnowButton3_text">
+            <h3>Family</h3>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -93,7 +152,7 @@ const moveAttractionDetail = () => {
   width: 120px;
   z-index: 2;
   display: none;
-  /* visibility: hidden; */
+  visibility: hidden;
 }
 
 #TripFestivalButton {
@@ -112,10 +171,53 @@ const moveAttractionDetail = () => {
   display: none;
 }
 
+.spotLight1 {
+  position: absolute;
+  transform: translate(260px, 160px);
+  border-radius: 100px;
+  height: 85px;
+  width: 85px;
+  box-shadow: 0 0px 60px rgb(52, 255, 228);
+}
+
+.img0 {
+  position: absolute;
+  transform: translate(150px, 110px);
+}
+
+#TripSnowButton0 {
+  position: absolute;
+  /* transform: translate(-350px, 110px); */
+  width: 150px;
+  outline: none;
+  transition: all ease 1s;
+  padding-top: 0;
+}
+
+#TripSnowButton0:hover {
+  position: absolute;
+  -ms-transform: rotate(360deg);
+  -webkit-transform: rotate(360deg);
+  transform: rotate(360deg);
+}
+
+.TripSnowButton0_text {
+  position: absolute;
+  /* transform: translate(213px, 195px); */
+  color: rgb(61, 71, 67);
+}
+
 #TripSnowButton1 {
   position: absolute;
   transform: translate(-250px, 150px);
   width: 100px;
+  outline: none;
+}
+
+.TripSnowButton1_text {
+  position: absolute;
+  transform: translate(280px, 175px);
+  color: rgb(61, 71, 67);
 }
 
 #TripSnowButton2 {
@@ -123,11 +225,21 @@ const moveAttractionDetail = () => {
   transform: translate(-50px, 100px);
   width: 100px;
 }
+.TripSnowButton2_text {
+  position: absolute;
+  transform: translate(470px, 125px);
+  color: rgb(61, 71, 67);
+}
 
 #TripSnowButton3 {
   position: absolute;
   transform: translate(150px, 150px);
   width: 100px;
+}
+.TripSnowButton3_text {
+  position: absolute;
+  transform: translate(670px, 170px);
+  color: rgb(61, 71, 67);
 }
 
 #trip {
