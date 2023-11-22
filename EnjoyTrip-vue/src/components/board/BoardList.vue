0b59d6736a73e1416e99097ef68a8e15dc7ed89c
강대은 <eodms4334@naver.com>
2023-11-22 10:14:44 +0900

    board 뷰 새로 만듦 꼭 성공하자

--- EnjoyTrip-vue/src/components/board/BoardList.vue
+++ EnjoyTrip-vue/src/components/board/BoardList.vue
@@ -4,9 +4,12 @@ import { useRoute, useRouter } from "vue-router";
 import { boardList, boardWrite, boardView, boardUpdateHit } from "@/api/board";
 import { useMemberStore } from "@/stores/member";
 import { storeToRefs } from "pinia";
+import BoardWriteModal from "@/components/board/BoardWriteModal.vue";
 
 const memberStore = useMemberStore();
 const { isLogin, memberInfo } = storeToRefs(memberStore);
+
+const isModalOpen = ref(false); //게시글 쓸 때 열 모달창
 
 const router = useRouter();
 const route = useRoute();
@@ -28,6 +31,23 @@ const getBoardList = () => {
 onMounted(() => {
   getBoardList();
 });
+
+/** 게시글 작성 - 모달창 */
+const showModal = (detail) => {
+  // 로그인이 되어있을 때에만 열리게 하기
+  if (isLogin.value) {
+    // getMypage();
+    isModalOpen.value = true;
+    // 모달이 나타날 때 show 클래스 추가
+    const modal = document.querySelector("#modal.modal-overlay");
+    modal.classList.add("show");
+  }
+};
+
+const moveBoardWrite = () => {
+  // mypage-bag 이미지를 클릭했을 때 모달창 열도록 설정
+  showModal();
+};
 </script>
 
 <template>
@@ -39,27 +59,111 @@ onMounted(() => {
   </div> -->
 
   <div class="board_background">
-    <h1>Hi</h1>
+    <div class="board_tree">
+      <div class="random_leaf">
+        <img class="board_leaf" src="@/assets/board/board_leaf_1.png" alt="" />
+        <img class="board_leaf" src="@/assets/board/board_leaf_2.png" alt="" />
+        <img class="board_leaf" src="@/assets/board/board_leaf_3.png" alt="" />
+        <img class="board_leaf" src="@/assets/board/board_leaf_4.png" alt="" />
+        <img class="board_leaf" src="@/assets/board/board_leaf_5.png" alt="" />
+        <img class="board_leaf" src="@/assets/board/board_leaf_6.png" alt="" />
+        <img class="board_leaf" src="@/assets/board/board_leaf_7.png" alt="" />
+        <img class="board_leaf" src="@/assets/board/board_leaf_8.png" alt="" />
+        <img class="board_leaf" src="@/assets/board/board_leaf_9.png" alt="" />
+        <img class="board_leaf" src="@/assets/board/board_leaf_10.png" alt="" />
+      </div>
+      <img
+        class="board_search"
+        src="@/assets/board/board_region_hole.png"
+        alt=""
+      />
+      <img
+        class="board_write"
+        src="@/assets/board/board_mindle_hole.png"
+        alt=""
+        @click="moveBoardWrite"
+      />
+    </div>
+  </div>
+
+  <div id="modal_div">
+    <!-- <button @click="showModal">모달 열기</button> -->
+    <teleport to="body" v-if="isModalOpen">
+      <BoardWriteModal></BoardWriteModal>
+    </teleport>
   </div>
 </template>
 
 
 <style scoped>
 .board_background {
-  background-image: url("@/assets/board/board_background6.jpeg");
+  /* background-image: url("@/assets/board/board_background6.jpeg"); */
+  background-color: darkblue;
   /* width: 1000px;
   height: 1000px; */
   background-size: cover;
   background-position: center;
   text-align: center;
-  margin: 0 auto;
   vertical-align: middle;
+  margin: -100px auto;
 
-  width: 90;
-  height: 250%;
+  /* width: 90; */
+  /* margin-top: 0px; */
+  width: 100%;
+  height: 220%;
+
+  /* visibility: hidden; */
 }
 
-.body {
-    background-color: white;
+.board_tree {
+  background-image: url("@/assets/board/board_tree.png");
+  background-size: cover;
+  height: 500px;
+  width: 500px;
+  margin: 0 auto;
+  position: relative; /* 부모(.board_background)에 대해 상대 위치로 지정. */
+  top: 60%; /* 부모(.board_background)의 세로 이동. */
+  transform: translateY(-50%); /* 자기 자신의 높이의 절반만큼 위로 이동. */
+
+  /* visibility: hidden; */
+}
+
+.board_leaf {
+  width: 100px;
+  z-index: 999;
+
+  /* visibility: hidden; */
+}
+
+.board_leaf:hover {
+  cursor: pointer;
+}
+
+.board_search {
+  position: absolute;
+  transform: translate(-560px, -50px) rotate(-50deg);
+  height: 100px;
+  cursor: pointer;
+  transition: transform 1s ease;
+
+  /* visibility: hidden; */
+  
+}
+
+.board_search:hover {
+  transform: translate(-560px, -50px) rotate(310deg);
+}
+
+.board_write {
+  width: 130px;
+  position: absolute;
+  transform: translate(-440px, 110px);
+  cursor: pointer;
+
+  /* visibility: hidden; */
+}
+
+.board_write:hover {
+  transform: translate(-440px, 110px) scale(1.2);
 }
 </style>
---
+++ EnjoyTrip-vue/src/components/board/BoardWriteModal.vue
@@ -0,0 +1,228 @@
+<script setup>
+import { ref, onMounted } from "vue";
+import { useMemberStore } from "@/stores/member";
+import { storeToRefs } from "pinia";
+
+const memberStore = useMemberStore();
+const { memberInfo, getMemberInfo, memberLogout } = memberStore;
+const { isLogin } = storeToRefs(memberStore);
+
+const isModalOpen = ref(false);
+
+const closeModal = () => {
+  isModalOpen.value = false;
+  // 모달을 닫을 때 show 클래스 제거
+  const modal = document.querySelector("#modal.modal-overlay");
+  if (modal) {
+    modal.classList.remove("show");
+  }
+};
+/** 모달창(디테일) 테스트 끝 */
+</script>
+
+<template>
+  <div id="modal" class="modal-overlay">
+    <div class="modal-window" @click.stop>
+      <div class="close-area" @click="closeModal">X</div>
+      <div class="title">
+        <!-- <h1>" Life is full of possibilities "</h1>
+        <h2>인생은 가능성으로 차 있어 - 소울-</h2> -->
+      </div>
+
+      <div class="content"></div>
+
+      <div class="board_input">
+        <label>제목</label>
+        <input id="title" type="text" required />
+        <textarea id="content" cols="25" rows="10" type="text" required />
+        <span></span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+/** 게시글 입력 */
+.board_input {
+  margin-top: 60px;
+}
+
+.board_input #title {
+  font-size: 15px;
+  color: #222222;
+  width: 300px;
+  border: none;
+  border-bottom: solid #aaaaaa 1px;
+  padding-bottom: 10px;
+  padding-left: 10px;
+  position: relative;
+  background: none;
+  z-index: 5;
+}
+
+.board_input input::placeholder {
+  color: #aaaaaa;
+}
+.board_input input:focus {
+  outline: none;
+}
+
+.board_input label {
+  /* position: absolute; */
+  color: rgb(107, 107, 107);
+  font-size: 20px;
+  bottom: 8px;
+  transition: all 0.2s;
+}
+
+.board_input input:focus ~ label,
+.board_input input:valid ~ label {
+  font-size: 16px;
+  bottom: 40px;
+  color: #666;
+  font-weight: bold;
+}
+
+.board_input input:focus ~ span,
+.board_input input:valid ~ span {
+  width: 100%;
+}
+
+/** 내용부분 */
+#content {
+  display: block;
+  position: relative;
+  margin: auto;
+  margin-top: 20px;
+  background: none;
+  z-index: 5;
+  font-size: 15px;
+  color: #222222;
+  width: 350px;
+  max-width: 100%; /* 최대 가로 너비 설정 */
+  height: 300px;
+  border-color: #aaaaaa;
+  border-radius: 2%;
+  border-style: ridge;
+  text-align: left;
+  overflow: auto; /* 내용이 넘칠 경우 스크롤 생성 */
+  word-wrap: break-word; /* 단어가 가로로 길어질 때 자동으로 줄 바꿈 */
+  padding: 10px; /* 내용 입력 영역에 패딩 추가 */
+  vertical-align: top; /* 텍스트가 상단에서부터 시작되도록 설정 */
+}
+
+.board_input input {
+  font-size: 15px;
+  color: #222222;
+  width: 100%; /* 입력 필드를 가로 전체로 확장 */
+  border: none;
+  border-bottom: solid #aaaaaa 1px;
+  padding-bottom: 10px;
+  padding-left: 10px;
+  position: relative;
+  background: none;
+  z-index: 5;
+  margin-bottom: 10px; /* 입력 필드 간격을 조절하기 위해 마진 추가 */
+}
+
+/** 모달 디자인 */
+.modal-window {
+  background-image: url("@/assets/board/board_leaf_10.png");
+  background-size: cover;
+  background-position: center;
+  text-align: center;
+  vertical-align: middle;
+  width: 600px;
+  height: 600px;
+  z-index: 999;
+}
+
+#modal .modal-window {
+  overflow-y: auto; /* 내용이 창을 벗어날 경우 스크롤 추가 */
+  max-height: 80vh; /* 모달창의 최대 높이를 지정하여 화면을 넘어가지 않도록 함 */
+}
+
+#modal.modal-overlay {
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: rgba(255, 255, 255, 0.25);
+  box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
+  backdrop-filter: blur(1.5px);
+  -webkit-backdrop-filter: blur(1.5px);
+  border-radius: 10px;
+  border: 1px solid rgba(255, 255, 255, 0.18);
+  /** 모달창 부드럽게 보이기 */
+  opacity: 0;
+  pointer-events: none;
+  transition: opacity 0.5s ease, transform 0.5s ease;
+  z-index: 999; /* 적절한 값으로 조절 */
+}
+
+#modal.modal-overlay.show {
+  opacity: 1;
+  pointer-events: auto;
+  transform: translate(-50%, -50%) scale(1); /* 이동 및 크기 변경 */
+}
+/* 
+#modal .modal-window {
+  background: rgba(0, 0, 0, 0.87);
+  box-shadow: 0 8px 32px 0 rgba(171, 175, 235, 0.37);
+  backdrop-filter: blur(13.5px);
+  -webkit-backdrop-filter: blur(13.5px);
+  border-radius: 10px;
+  border: 1px solid rgba(255, 255, 255, 0.18);
+  width: 900px;
+  height: 700px;
+  position: relative;
+  padding: 10px;
+} */
+#modal .title {
+  /* display: inline; */
+  padding-left: 250px;
+  text-shadow: 1px 1px 2px gray;
+  color: white;
+}
+#modal .title h2 {
+  padding-left: 55px;
+}
+#modal .close-area {
+  display: inline;
+  float: right;
+  padding-right: 10px;
+  cursor: pointer;
+  text-shadow: 1px 1px 2px gray;
+  color: white;
+}
+
+/** 모달창 예쁘게 */
+#modal .content {
+  margin-top: 20px;
+  align-content: center;
+  /* padding: 0px 10px; */
+  text-shadow: 1px 1px 2px rgb(68, 68, 68);
+  font-size: 20px;
+  color: rgb(255, 255, 255);
+  display: flex; /* Flexbox를 사용하여 이미지와 정보를 나란히 표시 !!!!!! (여기)*/
+  /* justify-content: space-between; */
+}
+/** 모달창(디테일) 테스트 끝 */
+
+/** 폰트 */
+@font-face {
+  font-family: "Bazzi";
+  src: url("https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_20-04@2.1/Bazzi.woff")
+    format("woff");
+  font-weight: normal;
+  font-style: normal;
+}
+* {
+  font-family: "Bazzi";
+}
+</style>
